ea701ca594b59661feb3a750a86a82ef55fe7364
Piotr Falkowski <piotr.falkowski@pl.abb.com>
2018-06-18 12:44:31 +0200

    More tests

--- src/components/Select/__tests__/index.js
+++ src/components/Select/__tests__/index.js
@@ -6,7 +6,31 @@ import Select from "../";
 describe("<Select/>", () => {
   it("should render without options", () => {
     const component = shallow(<Select/>);
+    expect(component.find('option')).toHaveLength(0);
   });
 
-  it("should render", () => {});
+  it("should render options", () => {
+    const options = [
+      {value: 'foo', text: 'Foo'},
+      {value: 'bar', text: 'Bar'}
+    ];
+
+    const component = shallow(<Select options={options}/>);
+
+    const selectOptions = component.find('option');
+    expect(selectOptions).toHaveLength(2);
+  });
+
+  it("should call onChange", () => {
+    const options = [
+      {value: 'foo', text: 'Foo'},
+      {value: 'bar', text: 'Bar'}
+    ];
+
+    const component = shallow(<Select options={options}/>);
+    const selectOptions = component.find('option');
+
+    expect(selectOptions).toHaveLength(2);
+  });
+
 });
